85cd655b276bcf40b5feaf427b89a99a75a3249e
qianlima <qianlima@tcl.com>
2019-08-28 17:12:29 +0800

    更新一些布局

--- app/src/main/java/com/alcatel/wifilink/root/widget/CountDownTextView.java
+++ app/src/main/java/com/alcatel/wifilink/root/widget/CountDownTextView.java
@@ -16,15 +16,14 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.widget.TextView;
 
-import com.alcatel.wifilink.R;
-
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
  * @author wdpm
- *         use handler and  message to implement count down
+ * use handler and  message to implement count down
  */
+@SuppressLint("AppCompatCustomView")
 public class CountDownTextView extends TextView {
     private Paint mPaint;
     private int mFontHeight = 0;
@@ -32,8 +31,8 @@ public class CountDownTextView extends TextView {
 
     private static final int DEFAULT_COUNT = 60;
     private static final Typeface DEFAULT_TYPE_FACE = Typeface.SERIF;
-    private static final int DEFAULT_TOP_COLOR = Color.BLACK;                                                // 默认颜色上部分黑色
-    private static final int DEFAULT_BOTTOM_COLOR = Color.BLACK;                                                // 默认颜色下部分黑色
+    private static final int DEFAULT_TOP_COLOR = Color.BLACK; // 默认颜色上部分黑色
+    private static final int DEFAULT_BOTTOM_COLOR = Color.BLACK; // 默认颜色下部分黑色
 
     private int mCount = DEFAULT_COUNT;
     private boolean isRunning = false;
@@ -64,53 +63,39 @@ public class CountDownTextView extends TextView {
 
     public CountDownTextView(Context context) {
         super(context);
-        init(null, null);
+        init(null);
     }
 
     public CountDownTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init(context, attrs);
-        //        TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.ScaleText);
-        //        mBottomColor = array.getColor(R.styleable.ScaleText_bottomColor, getTextColors().getDefaultColor());
-        //        mTopColor = array.getColor(R.styleable.ScaleText_topColor, getTextColors().getDefaultColor());
-        //        array.recycle();
-
+        init(attrs);
     }
 
     public CountDownTextView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init(context, attrs);
+        init(attrs);
     }
 
-    private void init(Context context, AttributeSet attrs) {
+    private void init(AttributeSet attrs) {
         if (attrs != null) {
-            readAttrs(context, attrs);
+            readAttrs(attrs);
         }
         mPaint = new Paint();
         setText(mText);
     }
 
-    private void readAttrs(Context context, AttributeSet attrs) {
-        int[] attrsArray = new int[]{android.R.attr.text, android.R.attr.textSize, R.attr.topColor, R.attr.bottomColor};
-
+    private void readAttrs(AttributeSet attrs) {
+        int[] attrsArray = new int[]{android.R.attr.text, android.R.attr.textSize};
         TypedArray ta = getContext().obtainStyledAttributes(attrs, attrsArray);
         mText = (String) ta.getText(0);
         if (TextUtils.isEmpty(mText)) {
             mText = "Begin";
         }
         mTextSize = ta.getDimension(1, mTextSize);
-        mTopColor = ta.getColor(2, mTopColor);
-        mBottomColor = ta.getColor(3, mBottomColor);
 
         ta.recycle();
-        this.setTypeface(mTypeface);
-        if (CanParseToInt(mText)) {
-            this.setCount(Integer.parseInt(mText));
-            Log.d("readAttrs", Integer.parseInt(mText) + "");
-
-        } else {
-            this.setCount(DEFAULT_COUNT);
-        }
+        setTypeface(mTypeface);
+        setCount(CanParseToInt(mText) ? Integer.parseInt(mText) : DEFAULT_COUNT);
     }
 
     private boolean CanParseToInt(String mText) {
@@ -153,19 +138,13 @@ public class CountDownTextView extends TextView {
     private final Handler mHandler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
-            Log.d("handleMessage", "arg1" + String.valueOf(msg.arg1));
-            Log.d("handleMessage", "arg2" + String.valueOf(msg.arg2));
-            Log.d("handleMessage", "describeContents" + String.valueOf(msg.describeContents()));
-            Log.d("handleMessage", "what" + String.valueOf(msg.what));
-            Log.d("handleMessage", "getTarget" + String.valueOf(msg.getTarget()));
-            Log.d("handleMessage", "getData" + String.valueOf(msg.getData()));
             switch (msg.what) {
                 case 1:
                     mCount--;
                     if (mCount < 0) {
                         return;
                     }
-                    CountDownTextView.this.setText(mCount + "");
+                    CountDownTextView.this.setText(String.valueOf(mCount));
                     invalidate();
                     Message message = mHandler.obtainMessage(1);
                     mHandler.sendMessageDelayed(message, 1000);
@@ -180,9 +159,7 @@ public class CountDownTextView extends TextView {
     };
 
     public void run() {
-        if (isRunning) {//true,represent count is running. no need to run repeatly
-            return;
-        } else {
+        if (!isRunning) {//true,represent count is running. no need to run repeatly
             isRunning = true;// set run state true
             Message message = mHandler.obtainMessage(1);
             mHandler.sendMessage(message);
@@ -199,13 +176,12 @@ public class CountDownTextView extends TextView {
     public void reset() {
         if (isRunning) {
             mHandler.removeMessages(1);
-        } else {
-            //don't need to remove message
         }
+
         isRunning = false;
         mCount = mUserCount;
         Log.d("reset", "mCount:" + mCount);
-        CountDownTextView.this.setText(mUserCount + "");
+        CountDownTextView.this.setText(String.valueOf(mUserCount));
         invalidate();
     }
 
